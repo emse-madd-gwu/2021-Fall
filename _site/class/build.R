ea0a89bca0825ec51ece60797c602ba850ceb887
John Helveston <john.helveston@gmail.com>
2021-07-19 20:00:45 -0400

    finished draft of schedule

---
+++ _site/class/build.R
@@ -0,0 +1,15 @@
+lesson <- strsplit(here::here(), "/")[[1]]
+lesson <- lesson[length(lesson)]
+
+# Build the slides
+xaringanBuilder::build_html("index.Rmd", "index.html")
+xaringanBuilder::build_pdf("index.html", paste0(lesson, ".pdf"))
+
+# Create zip files of class notes
+zip::zip(
+    zipfile = paste0(lesson, ".zip"),
+    files = c(
+        'data',
+        'notes-blank.Rmd',
+        'notes-complete.Rmd',
+        paste0(lesson, ".Rproj")))
