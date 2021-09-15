a00f174906fd4d9643df26b4194ca6364bacedce
jhelvy <john.helveston@gmail.com>
2021-09-15 20:52:09 +0000

    Deploying to gh-pages from  @ b0536ab1cd842178bf6687f4825cf01a0fc5bb40 🚀

--- class/build.R
+++ class/build.R
@@ -6,10 +6,13 @@ xaringanBuilder::build_html("index.Rmd", "index.html")
 xaringanBuilder::build_pdf("index.html", paste0(lesson, ".pdf"))
 
 # Create zip files of class notes
-zip::zip(
+zip::zipr(
     zipfile = paste0(lesson, ".zip"),
     files = c(
-        'data',
-        'notes.R',
-        'notes-solutions.R',
+        "bears_solutions.Rmd",
+        "bears.Rmd",
+        "data",
+        "ggplot2_solutions.Rmd",
+        "ggplot2.Rmd",
+        "rmd_demo.Rmd",
         paste0(lesson, ".Rproj")))
