2ee565021239ebcb6dc593ac416034439ed7b5ef
jhelvy <john.helveston@gmail.com>
2021-11-03 20:32:47 +0000

    Deploying to gh-pages from  @ cc0dfeaa1ffb579212e616e7a4e844e8d594f40e 🚀

--- class/build.R
+++ class/build.R
@@ -9,10 +9,14 @@ xaringanBuilder::build_pdf("index.html", paste0(lesson, ".pdf"))
 zip::zipr(
     zipfile = paste0(lesson, ".zip"),
     files = c(
-        'data',
+        'cars.Rmd',
+        'efficiency.Rmd',
+        'powerAnalysis.Rmd',
+        'powerAnalysis_interactions.Rmd',
         'practice.Rmd',
         'solutions.html',
         'solutions.Rmd',
+        'winePairings.Rmd',
         paste0(lesson, ".Rproj")))
 
 
